02222eb075caf099bac596d348290639382a25ab
fengwei <fengwei168@aliyun.com>
2018-09-23 01:15:34 +0800

    添加推荐保存

--- test/run.sh
+++ test/run.sh
@@ -3,3 +3,5 @@
 /usr/bin/php /home/data/wwwroot/glidersky/test/job.php -btest\model\UsChinaPriceModel -aurdgt -n3
 /usr/bin/php /home/data/wwwroot/glidersky/test/job.php -btest\model\UsChinaPriceModel -asraug -n5
 /usr/bin/php /home/data/wwwroot/glidersky/test/job.php -btest\model\UsChinaPriceModel -asraul -n5
+
+;;16 1 * * * /usr/bin/sh /home/data/wwwroot/glidersky/test/run.sh > /dev/null 2>&1 &
