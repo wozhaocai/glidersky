aff9c7cc5d02f4a8683bc8aea308b5ecc7aed164
fengwei <fengwei168@aliyun.com>
2018-11-13 21:16:25 +0800

    修改股票信息

--- test/allshow.php
+++ test/allshow.php
@@ -21,12 +21,14 @@ $params = [
 ];
 $store = new \GliderSky\framework\data\StoreService("file", $params);
 $aCodePrice = array();
+$aCodeAllInfo = array();
 
 foreach ($codeList as $row) {
     $spider->setParams($row);
     $rs = $spider->run('', false);
     if ($rs) {
-        $aCodePrice[$rs["code"]] = $rs["up_rate"];
+        $sStr = "|股票名称:".$rs["cname"]."&nbsp;&nbsp;涨幅:".$rs["up_rate"]."%";
+        $aCodePrice[$rs["code"].$sStr] = $rs["up_rate"];
     }
 }
 
